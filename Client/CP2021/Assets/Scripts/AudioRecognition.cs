5a5c148738aea7fd3126475392850de79abc119f
Kirill Khrustitskii <mycapp@gmail.com>
2021-08-21 11:42:48 +0400

    Добавлено Readme

---
+++ Client/CP2021/Assets/Scripts/AudioRecognition.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioRecognition : MonoBehaviour
+{
+    [SerializeField] private AudioClip ac;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
